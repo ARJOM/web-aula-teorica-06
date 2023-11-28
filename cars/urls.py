2018ac86bcc0124b713fc52cef0a51359c39f012
Antônio Ricart <ricarteusuo11@gmail.com>
2023-11-28 11:06:17 -0300

    Adiciona crud de carro

--- cars/urls.py
+++ cars/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from cars.views import ListMarca, TemplateHome, CreateMarca, UpdateMarca, DeleteMarca
+from cars.views import ListMarca, TemplateHome, CreateMarca, UpdateMarca, DeleteMarca, ListCarro, CreateCarro, \
+    UpdateCarro, DeleteCarro
 
 urlpatterns = [
-    path('', TemplateHome.as_view() , name='home'),
+    path('', TemplateHome.as_view(), name='home'),
     path('marca/', ListMarca.as_view(), name='list-marca'),
     path('marca/new/', CreateMarca.as_view(), name='create-marca'),
     path('marca/edit/<int:pk>', UpdateMarca.as_view(), name='update-marca'),
     path('marca/delete/<int:pk>', DeleteMarca.as_view(), name='delete-marca'),
+    path('carro/', ListCarro.as_view(), name='list-carro'),
+    path('carro/new/', CreateCarro.as_view(), name='create-carro'),
+    path('carro/edit/<int:pk>', UpdateCarro.as_view(), name='update-carro'),
+    path('carro/delete/<int:pk>', DeleteCarro.as_view(), name='delete-carro'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, TemplateView, CreateView, UpdateView, DeleteView
 
-from cars.models import Marca
+from cars.models import Marca, Carro
 
 
 class TemplateHome(TemplateView):
@@ -29,3 +29,24 @@ class UpdateMarca(UpdateView):
 class DeleteMarca(DeleteView):
     model = Marca
     success_url = reverse_lazy('list-marca')
+
+
+# Carro
+class CreateCarro(CreateView):
+    model = Carro
+    fields = ('nome', 'modelo', 'marca', 'dono',)
+    success_url = reverse_lazy('list-carro')
+
+class ListCarro(ListView):
+    model = Carro
+
+
+class UpdateCarro(UpdateView):
+    model = Carro
+    fields = ('nome', 'modelo', 'marca', 'dono',)
+    success_url = reverse_lazy('list-carro')
+
+
+class DeleteCarro(DeleteView):
+    model = Carro
+    success_url = reverse_lazy('list-carro')
